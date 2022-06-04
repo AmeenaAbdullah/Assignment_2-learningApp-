d7eefb6fb35c8f9d6c374ad204379a5b6cdfa4de
Ameena <meenamughal886@gmail.com>
2022-06-04 15:30:58 -0700

    updated

--- app/src/main/java/com/example/lets_learn/MainActivity.java
+++ app/src/main/java/com/example/lets_learn/MainActivity.java
@@ -49,10 +49,7 @@ public class MainActivity extends AppCompatActivity {
 
         Button q=findViewById(R.id. quiz);
        q.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this,numbers.class));
-            }
+          
         });
     }
 }
